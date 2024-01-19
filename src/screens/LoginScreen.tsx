a86664bfe2ae267f82e06a6f24190685d09a0e1e
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2024-01-19 23:20:51 +0530

    Login page

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,15 @@
-import { Text, TouchableOpacity, View, Image } from 'react-native'
+import { Text, TouchableOpacity, View, Image, TextInput } from 'react-native'
 import React from 'react'
 import { themeColors } from '../../themes'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import relax from "../../assets/images/relax.png"
+import google from "../../assets/images/google.png"
+import apple from "../../assets/images/apple.png"
+import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
+
+
 
 type Props = {}
 
@@ -24,6 +29,59 @@ const LoginScreen = (props: Props) => {
           <Image source={relax} style={{width:200, height:200}}/>
         </View>
       </SafeAreaView>
+      <View className=" flex-1 bg-white px-8 pt-8 pb-8" style={{borderTopLeftRadius:50, borderTopRightRadius:50}}>
+          <GestureHandlerRootView>
+            <ScrollView showsVerticalScrollIndicator={false}>
+            <View className="form space-y-2">
+            <Text className="text-gray-700 ml-4">Email Address</Text>
+            <TextInput 
+              className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+              placeholder="email"
+              value="john@gmail.com" 
+            />
+            <Text className="text-gray-700 ml-4">Password</Text>
+            <TextInput 
+              className="p-4 bg-gray-100 text-gray-700 rounded-2xl"
+              secureTextEntry
+              placeholder="password"
+              value="test12345" 
+            />
+            <TouchableOpacity className="flex items-end">
+              <Text className="text-gray-700 mb-5">Forgot Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              className="py-3 bg-yellow-400 rounded-xl">
+                <Text 
+                    className="text-xl font-bold text-center text-gray-700"
+                >
+                        Login
+                </Text>
+             </TouchableOpacity>
+            
+            </View>
+            <Text className="text-xl text-gray-700 font-bold text-center py-5">Or</Text>
+            <View className="flex-row justify-center space-x-12">
+              <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+                <Image source={google} className="w-10 h-10" />
+              </TouchableOpacity>
+              <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+                <Image source={apple} className="w-10 h-10" />
+              </TouchableOpacity>
+              {/* <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+                <Image source={require('../assets/icons/facebook.png')} className="w-10 h-10" />
+              </TouchableOpacity> */}
+            </View>
+            <View className="flex-row justify-center mt-7">
+                <Text className="text-gray-500 font-semibold">
+                    Don't have an account?
+                </Text>
+                <TouchableOpacity onPress={()=> navigation.navigate('Signup')}>
+                    <Text className="font-semibold text-yellow-500"> Sign Up</Text>
+                </TouchableOpacity>
+            </View>
+            </ScrollView>
+          </GestureHandlerRootView>
+      </View>
     </View>
   )
 }
